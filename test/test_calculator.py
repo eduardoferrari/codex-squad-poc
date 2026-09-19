e3eb3628dbe01150664a0cf20bcdd0ce87ab7ffb
Eduardo Ferrari <eduardoferrari@gmail.com>
2026-09-19 21:54:49 +0200

    Add multiplication support

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,5 +1,14 @@
-from src.calculator import add
+from src.calculator import add, multiply
 
 
 def test_add():
     assert add(2, 3) == 5
+
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+
+def test_multiply_with_zero_and_negative_number():
+    assert multiply(0, 10) == 0
+    assert multiply(-2, 3) == -6
